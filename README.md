34a5341c85ce5beae68b0e08087b4eca87fcc299
Andre Hiroshi da Silva <hiroshisilva@gmail.com>
2018-02-04 23:52:43 -0200

    Documentação do Readme e a criação do método main

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# search moves in file
+# search moves in file #
+
+Esse projeto faz buscas de filmes indexando os dados apartir de arquivos.
+
+# pré requisitos #
+- Java (JDK-8)
+- Pasta data dentro do diretório de execução do Jar
+
+
+### Build.###
+Para efetuar o Build basta digitar a linha abaixo dentro do diretório do projeto.
+
+Linux:
+`./gradlew build packageJar`
+
+Windows:
+`gradle.bat build packageJar`
+
+
+### Execução: ###
+
+* Executar o projeto:
+    * Copie os arquivos a serem indexados para a pasta data dentro do mesmo diretório a onde o jar será executado.
+    * Copie o jar gerado dentro do diretório build lib para o mesmo diretório da pasta data.
+    * Execute o código abaixo:
+    ` java -jar SearchMovieInDataFile-1.0-SNAPSHOT.jar "walt disney"`
+
+### Analise de tempo de execução ###
+ Após a execução do jar será gerado um arquivo searchMovie.log no mesmo diretório do jar.
+ O arquivo searchMovie.log contem o log com os tempos de execução tanto do carregamento dos dados, quanto da busca,
+ a busca estará tagada como `search data`
--- src/main/java/search/movie/Main.java
+++ src/main/java/search/movie/Main.java
@@ -1,8 +1,53 @@
 package search.movie;
 
+
+import org.apache.log4j.Logger;
+import org.perf4j.StopWatch;
+import org.perf4j.log4j.Log4JStopWatch;
+import search.movie.reader.FileReader;
+import search.movie.reader.Reader;
+import search.movie.search.SearchMovie;
+
+import java.io.IOException;
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args){
+    private final static Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) throws IOException {
+
+        if(args.length > 0) {
+
+
+            StringTokenizer tokenizer = new StringTokenizer(args[0]);
+            List<String> tokens = new ArrayList<>();
+            while(tokenizer.hasMoreTokens()) {
+                tokens.add(tokenizer.nextToken().trim());
+            }
+
+            Reader reader = new FileReader("data");
+            Map<String, Set<String>> dataset;
+
+            StopWatch stopWatchReader = new Log4JStopWatch("load Data");
+            dataset = reader.readData();
+            stopWatchReader.stop("load Data");
+
+            System.out.println(String.format("Foram carregados %d registros",dataset.size()));
+
+            SearchMovie searchMovie = new SearchMovie();
+
+            StopWatch stopWatchSearch = new Log4JStopWatch("search data");
+            Set<String> result = searchMovie.find(dataset, tokens.toArray(new String[0]));
+            stopWatchSearch.stop("search data");
+
+            System.out.println(String.format("Foram encontrados %d registros",result.size()));
+            result.parallelStream().forEach(System.out::println);
+
+
+        }else{
+            System.out.println("Não foram passados os dados de busca!");
+        }
 
     }
 
